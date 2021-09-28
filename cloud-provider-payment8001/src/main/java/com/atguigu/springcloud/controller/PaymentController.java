4f77e3704a54c25065f99125ef8c4a7a2586aed7
Haitao Gu <haitao.gu@samsclubs.com>
2021-09-28 05:19:27 -0400

    finish the 8081

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,43 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(Payment payment) {
+        int result = paymentService.create(payment);
+        log.info("*****inserted: " + result);
+
+        if(result > 0) {
+            return new CommonResult(200, "insert into db successfully", result);
+        } else {
+            return new CommonResult(444, "insert failed", null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("do query" + "hahahaaaa");
+        if(payment != null) {
+            return new CommonResult(200, "query successfully", payment);
+        } else {
+            return new CommonResult(444, "query failed with id: " + id, null);
+        }
+    }
+}
